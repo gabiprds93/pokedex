8743e68fd92327c04a2c12390d268a8a6698a276
Gabriela Paredes <gabiprds.93@gmail.com>
2021-10-16 16:23:10 -0500

    add responsive for cards

--- src/components/home/PokemonList/PokemonList.styles.tsx
+++ src/components/home/PokemonList/PokemonList.styles.tsx
@@ -2,6 +2,10 @@ import styled from "styled-components";
 
 // Types
 import { PokemonListStyledProps as Props } from "./PokemonList.types";
+// Configs
+import CONSTANTS from "../../../configs/constants";
+
+const { desktop, tablet, mobile } = CONSTANTS.BREAKPOINTS;
 
 const PokemonListStyled = styled.div<Props>`
   &.PokemonList {
@@ -17,6 +21,18 @@ const PokemonListStyled = styled.div<Props>`
       display: grid;
       grid-template-columns: repeat(4, 20rem);
       gap: 2.4rem;
+
+      @media screen and (max-width: ${desktop}px) {
+        grid-template-columns: repeat(3, 20rem);
+      }
+
+      @media screen and (max-width: ${tablet}px) {
+        grid-template-columns: repeat(2, 20rem);
+      }
+
+      @media screen and (max-width: ${mobile}px) {
+        grid-template-columns: repeat(1, 20rem);
+      }
     }
 
     &__refresh {
---
+++ src/configs/constants.ts
@@ -0,0 +1,13 @@
+// Project constants
+
+const CONSTANTS = {
+  BREAKPOINTS: {
+    mobile: 480,
+    tablet: 768,
+    desktop: 1024,
+    wide: 1800,
+    hd: 2652,
+  },
+};
+
+export default CONSTANTS;
