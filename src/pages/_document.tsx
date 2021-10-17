34002d150d2bb7deeb069aee0f0f558a6d618bb0
Gabriela Paredes <gabiprds.93@gmail.com>
2021-10-17 14:17:46 -0500

    change _document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,52 +1,88 @@
-import React from "react";
-import Document from "next/document";
-import { DocumentContext, Head, Html, Main, NextScript } from "next/document";
-import { NextPageContext } from "next";
-import { ServerStyleSheet } from "styled-components";
+import { Head, Html, Main, NextScript } from "next/document";
 
 // Configs
 import CONSTANTS from "../configs/constants";
 
 const { API_URL } = CONSTANTS;
 
-interface Context extends NextPageContext {
-  // any modifications to the default context, e.g. query types
-  renderPage: (config: { enhanceApp: any }) => void;
+function dedupe(bundles: any[]) {
+  const files = new Set<string>();
+  const kept = [];
+
+  for (const bundle of bundles) {
+    if (files.has(bundle.file)) continue;
+    files.add(bundle.file);
+    kept.push(bundle);
+  }
+  return kept;
 }
 
-interface GetInitialPropsRes {
-  styles: JSX.Element;
-  html: string;
-  head?: (JSX.Element | null)[] | undefined;
-}
+type DocumentFiles = {
+  sharedFiles: readonly string[];
+  pageFiles: readonly string[];
+  allFiles: readonly string[];
+};
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: Context): Promise<GetInitialPropsRes> {
-    const sheet = new ServerStyleSheet();
-    const originalRenderPage = ctx.renderPage;
+/**
+ * Custom NextScript to defer loading of unnecessary JS.
+ * Standard behavior is async. Compatible with Next.js 10.0.3
+ */
+class DeferNextScript extends NextScript {
+  getDynamicChunks(files: DocumentFiles) {
+    const {
+      dynamicImports,
+      assetPrefix,
+      isDevelopment,
+      devOnlyCacheBusterQueryString,
+    } = this.context;
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: (App: any) => (props: any) =>
-            sheet.collectStyles(<App {...props} />),
-        });
+    return dedupe(dynamicImports).map((bundle) => {
+      if (!bundle.file.endsWith(".js") || files.allFiles.includes(bundle.file))
+        return null;
 
-      const initialProps = await Document.getInitialProps(
-        ctx as DocumentContext
+      return (
+        <script
+          defer={!isDevelopment}
+          key={bundle.file}
+          src={`${assetPrefix}/_next/${encodeURI(
+            bundle.file
+          )}${devOnlyCacheBusterQueryString}`}
+          nonce={this.props.nonce}
+          crossOrigin={
+            this.props.crossOrigin || process.env.__NEXT_CROSS_ORIGIN
+          }
+        />
       );
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      };
-    } finally {
-      sheet.seal();
-    }
+    });
+  }
+  getScripts(files: DocumentFiles) {
+    const {
+      assetPrefix,
+      buildManifest,
+      isDevelopment,
+      devOnlyCacheBusterQueryString,
+    } = this.context;
+
+    const normalScripts = files.allFiles.filter((file) => file.endsWith(".js"));
+    const lowPriorityScripts = buildManifest.lowPriorityFiles?.filter((file) =>
+      file.endsWith(".js")
+    );
+
+    return [...normalScripts, ...lowPriorityScripts].map((file) => {
+      return (
+        <script
+          key={file}
+          src={`${assetPrefix}/_next/${encodeURI(
+            file
+          )}${devOnlyCacheBusterQueryString}`}
+          nonce={this.props.nonce}
+          defer={!isDevelopment}
+          crossOrigin={
+            this.props.crossOrigin || process.env.__NEXT_CROSS_ORIGIN
+          }
+        />
+      );
+    });
   }
 
   render() {
@@ -66,4 +102,4 @@ class MyDocument extends Document {
   }
 }
 
-export default MyDocument;
+export default DeferNextScript;
